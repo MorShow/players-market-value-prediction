ec3ed9634dc526a28933070a851797aab639bac7
aleks <aleksandrkasatmail@gmail.com>
2024-07-23 22:20:13 +0200

    Web server and interaction with the user

--- page/app.py
+++ page/app.py
@@ -3,14 +3,42 @@ import dill
 import pandas as pd
 import numpy as np
 
-COLUMNS_COUNT = 45
+COLUMNS_COUNT = 10
 
 NAMES_MAPPING = {
     'age': 'Age',
     'goals': 'Goals',
     'assists': 'Assists',
     'rank': 'Team_rank',
-    'rating': 'Rating'
+    'rating': 'Rating',
+    'mins': 'Mins',
+    'ps': 'PS',
+    'popularity': 'Popularity',
+    'avgp': 'AvgP',
+    'epl': 'From EPL'
+}
+
+COLUMNS_TO_MAP = ['rank', 'goals', 'assists', 'popularity', 'epl']
+
+MAPPINGS = {
+    'rank_a': 0,
+    'rank_b': 1,
+    'rank_c': 2,
+    'goals_7': 0,
+    'goals_14': 1,
+    'goals_21': 2,
+    'goals_28': 3,
+    'assists_3': 0,
+    'assists_7': 1,
+    'assists_10': 2,
+    'assists_14': 3,
+    'popularity_1': 0,
+    'popularity_2': 1,
+    'popularity_3': 2,
+    'popularity_4': 3,
+    'popularity_5': 4,
+    'epl_false': False,
+    'epl_true': True
 }
 
 app = Flask(__name__)
@@ -31,6 +59,9 @@ def enter():
     player_index = len(table.index) - 1
 
     for key, value in values_dictionary.items():
+        if key in COLUMNS_TO_MAP:
+            value = MAPPINGS.get(value)
+
         key = NAMES_MAPPING.get(key)
         table.loc[player_index, key] = value
 
