e394912d3993ebe6163feeb9d1a42829dcb3ea34
aleks <aleksandrkasatmail@gmail.com>
2024-07-24 22:20:19 +0200

    Mapping for online data preprocessing

---
+++ page/page_mappings.py
@@ -0,0 +1,35 @@
+NAMES_MAPPING = {
+    'age': 'Age',
+    'goals': 'Goals',
+    'assists': 'Assists',
+    'rank': 'Team_rank',
+    'rating': 'Rating',
+    'mins': 'Mins',
+    'ps': 'PS',
+    'popularity': 'Popularity',
+    'avgp': 'AvgP',
+    'epl': 'From EPL'
+}
+
+COLUMNS_TO_MAP = ['rank', 'goals', 'assists', 'popularity', 'epl']
+
+MAPPINGS = {
+    'rank_a': 0,
+    'rank_b': 1,
+    'rank_c': 2,
+    'goals_7': 0,
+    'goals_14': 1,
+    'goals_21': 2,
+    'goals_28': 3,
+    'assists_3': 0,
+    'assists_7': 1,
+    'assists_10': 2,
+    'assists_14': 3,
+    'popularity_1': 0,
+    'popularity_2': 1,
+    'popularity_3': 2,
+    'popularity_4': 3,
+    'popularity_5': 4,
+    'epl_false': False,
+    'epl_true': True
+}
